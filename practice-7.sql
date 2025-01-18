4327c4fd701e315b2a71535c71aa0e20cde7a6df
Kajalkaran Shah <drkajal.sh@gmail.com>
2025-01-18 22:04:26 +0000

    practice7

--- practice-7.sql
+++ practice-7.sql
@@ -15,5 +15,9 @@
 -- | Entire serviced apartment           | 1271     | 2021-10-17                 |
 -- | Entire townhouse                    | 3588     | 2021-10-17                 |
 -- | Entire villa                        | 75       | 2021-10-12                 |
-
+SELECT property_type, COUNT(reviews.id), MAX(reviews.date_reviewed)
+FROM listings
+INNER JOIN reviews ON listings.id = reviews.listing_id
+GROUP BY property_type
+ORDER BY property_type ASC
 
